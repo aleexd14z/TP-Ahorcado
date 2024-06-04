67ab9b6bdf5a0fe494e20c8aa732ee224d222948
Alex Paiano <alexpaiano14@outlook.com>
2024-06-03 21:42:36 -0300

    Errar palabra

--- TDD/UnitTest1.cs
+++ TDD/UnitTest1.cs
@@ -9,5 +9,13 @@ namespace TDD
             var resultado = juego.ArriesgarPalabra("palabra");
             Assert.True(resultado);
         }
+
+        [Fact]
+        public void ErrarPalabraDentroDe7Intentos()
+        {
+            var juego = new TP_Ahorcado.Ahorcado("palabra");
+            var resultado = juego.ArriesgarPalabra("palabras");
+            Assert.True(resultado);
+        }
     }
 }
